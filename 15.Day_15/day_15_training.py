1871d5d6a2fa7f734d478ccd20bccfd6036757fe
Diaconu Evelin <diaconuevelindanut@gmail.com>
2023-09-13 13:20:26 +0300

    Modifing the methods inside the class so that they dont return values.

--- 15.Day_15/day_15_training.py
+++ 15.Day_15/day_15_training.py
@@ -16,59 +16,57 @@ class CoffeeMachine:
         self.resources = resources
         self.coffee_options = coffee_options
         self.coins = coins
+        self.flag_check_coins = None
+        self.choice = None
+        self.offerings = None
 
-    def your_coffee_choices(self) -> str:
-        choice = input("What would you like to drink today my friend?: ")
-        return choice
+    def your_coffee_choices(self) -> None:
+        """Asks you for what would you like to drink."""
+        self.choice = input("What would you like to drink today my friend?: ")
 
     def report(self):
         """This method generates a report with the remaining resources available inside the machine."""
         for resource, quantity in self.resources.items():
             print(f"-- {resource}: {quantity} --")
 
-    def coffee_offerings(self) -> str:
-        offerings = " | ".join(
+    def coffee_offerings(self) -> None:
+        """This method show you the offerings ofthe coffe machine in a dinamic way."""
+        self.offerings = " | ".join(
             [
                 f"{coffee} (${self.coffee_options[coffee]['cost']:.2f})"
                 for coffee in self.coffee_options.keys()
             ]
         )
-        return offerings
 
     def turning_off(self):
         """Exists the program aka closes the machine."""
         print("Turning Off! Goodbye")
         sys.exit()
 
-    def check_resources(self, choice: str) -> bool:
-        # verifica resurse si are nevoie de cafeaua aleasa
+    def check_resources(self) -> bool:
         """Checks for the resources based on your choice.
         Args:
             choice (str): Takes in your choice made in the prompt method.
         Returns:
             bool: Makes your coffee or not based on the resources.
         """
-        if choice not in self.coffee_options:
+        if self.choice not in self.coffee_options:
             print("Invalid Choice. Please choose from the available options.")
-            return False
-        for resource, quantity_required in self.coffee_options[choice][
-            "ingredients"
-        ].items():
-            if quantity_required > self.resources.get(resource, 0):
-                print(f"Insufficient {resource}. Cannot make {choice}")
-                return False
-        return True
-
-    def check_coins(self, choice, coins):
-        if self.coffee_options[choice]["cost"] > coins:
-            return False
         else:
-            return True
+            for resource, quantity_required in self.coffee_options[self.choice][
+                "ingredients"
+            ].items():
+                if quantity_required > self.resources.get(resource, 0):
+                    print(f"Insufficient {resource}. Cannot make {self.choice}")
+                    return False
+                else:
+                    return True
+
+    def check_coins(self):
+        return self.coffee_options[self.choice]["cost"] > self.coins
 
     def making_coffee(self, choice: str) -> dict:
-        # face cafeaua adica scade din resursele introduse costul cafelei tale
         """Makes the actual coffee.
-
         Args:
             choice (str): Takes in your choice and makes your coffee
         """
@@ -76,10 +74,9 @@ class CoffeeMachine:
             "ingredients"
         ].items():
             self.resources[resource] = self.resources[resource] - quantity_required
-        return self.resources[resource]
 
     def money_inserted(self) -> int:
-        # returneaza coins pe care le-ai bagat in aparat
+
         """This is the balances(money) aka register
 
         Args:
@@ -88,7 +85,6 @@ class CoffeeMachine:
         Returns:
             int: Your change.
         """
-
         # quarters = 25 cents
         quarters = float(input("Please input how many quarters (25 cents): "))
         # dimes = 10 cents
@@ -98,7 +94,6 @@ class CoffeeMachine:
         # penni = 1 cent
         penni = float(input("Please input how many pennies (1 cents): "))
         self.coins = quarters * 0.25 + dimes * 0.10 + nickel * 0.05 + penni * 0.01
-        return self.coins
 
 
 cafea = CoffeeMachine(coffee_options=coffee_data, resources=initial_resources)
@@ -109,8 +104,9 @@ cafea = CoffeeMachine(coffee_options=coffee_data, resources=initial_resources)
 
 # check_resursa = cafea.check_resources(choice="latte")
 cafea.report()
+ce_bei = cafea.your_coffee_choices()
 
-fa_cafea = cafea.making_coffee(choice="latte")
+print(cafea.check_resources())
 
 cafea.report()
 # print(coins_check)
