33b72180f7cf2295bbbe88ad1333887eb044a6ce
Diaconu Evelin <diaconuevelindanut@gmail.com>
2023-12-22 12:09:35 +0200

    Storage update to json

--- 29.Day_29/day_29_training.py
+++ 29.Day_29/day_29_training.py
@@ -4,6 +4,8 @@ from tkinter import messagebox
 from PIL import Image, ImageTk
 import string
 import random
+import json
+import json
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -18,8 +20,14 @@ class PasswordManager(tk.Tk):
         self.ui_setup()
         
     
+
+        
   
-    
+    def clear_entries(self):
+        self.website_entry.delete(0, tk.END)
+        self.email_entry.delete(0, tk.END)
+        self.password_entry.delete(0, tk.END)
+        self.website_entry.focus()
 
     def check_website_length(self):
         website = self.website_entry.get()
@@ -28,19 +36,6 @@ class PasswordManager(tk.Tk):
             return True
         return False
 
-
-    def check_email_match(self):
-        website = self.website_entry.get()
-        self.matching_entries = self.read_entries_from_file(website)
-
-        if self.matching_entries:
-            if len(self.matching_entries) > 1:
-                self.open_selection_window()
-            else:
-                self.insert_matching_entry(self.matching_entries[0])
-                tk.messagebox.showinfo(title="Match Found", message="Match found for the website.")
-        else:
-            tk.messagebox.showinfo(title="No Match Found", message="No entries found for the website.")
 
     def open_selection_window(self):
         self.selection_window = tk.Toplevel(self)
@@ -80,11 +75,8 @@ class PasswordManager(tk.Tk):
         listbox.configure(border=0, highlightthickness=0, justify=tk.CENTER)
 
     def populate_listbox(self, listbox):
-        # Populate the Listbox with email entries
         for entry in self.matching_entries:
-            email = entry.split("|")[1].strip()
-            listbox.insert(tk.END, email)
-
+            listbox.insert(tk.END, entry['email'])
 
 
     def on_select(self, evt):
@@ -92,72 +84,109 @@ class PasswordManager(tk.Tk):
         index = int(w.curselection()[0])
         selected_entry = self.matching_entries[index]
         self.insert_matching_entry(selected_entry)
+        
         self.selection_window.destroy()
-        tk.messagebox.showinfo(title="Match Found", message="Match found for the website.")
+        
 
 
-    def read_entries_from_file(self, website):
-        with open("29.Day_29/data.txt", mode="r") as file:
-            data = file.readlines()
-            matching_entries = []
-            for line in data:
-                entry = line.split("|")
-                if website.lower() == entry[0].strip().lower():
-                    matching_entries.append(line)
-                    
-        return matching_entries
 
     def insert_matching_entry(self, entry):
-        self.website_entry.delete(0, tk.END)
         self.email_entry.delete(0, tk.END)
-        self.password_entry.delete(0, tk.END)
-        self.website_entry.insert(0, entry.split("|")[0].strip())
-        self.email_entry.insert(0, entry.split("|")[1].strip())
-        self.password_entry.insert(0, entry.split("|")[2].strip())
+        self.password_entry.delete(0, tk.END)      
+        self.email_entry.insert(0, entry["email"])
+        self.password_entry.insert(0, entry["password"])
+       
 
 
-    def lookup_entry(self):
-        if self.check_website_length():
-            return
-        self.check_email_match()
 
-    
+
+
+
+
     def generate_password(self):
         characters = string.ascii_letters + string.digits + string.punctuation
         password = "".join(random.choice(characters) for _ in range(15))
         self.password_entry.delete(0, tk.END)
         self.password_entry.insert(0, password)
-        
-    def add_entry(self):
-        website = self.website_entry.get()
-        email = self.email_entry.get()
-        password = self.password_entry.get()
-
-        if len(website) == 0 or len(password) == 0 or len(email) == 0:
-            tk.messagebox.showinfo(title="Error!", message="Please don't leave any fields empty!")
-        else:
-            is_duplicate = self.check_duplicate_entry(website, email)
-            if is_duplicate:
-                tk.messagebox.showinfo(title="Duplicate Entry", message="This entry already exists!")
-            else:
-                is_ok = tk.messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \nIs it ok to save?")
-                if is_ok:
-                    with open("29.Day_29/data.txt", mode="a") as file:
-                        file.write(f"{website} | {email} | {password}\n")
-                        self.website_entry.delete(0, tk.END)
-                        self.password_entry.delete(0, tk.END)
-                        self.website_entry.focus()
-
-    def check_duplicate_entry(self, website, email):
-        with open("29.Day_29/data.txt", mode="r") as file:
-            data = file.readlines()
-            for line in data:
-                entry = line.split("|")
-                if website.lower() == entry[0].strip().lower() and email.lower() == entry[1].strip().lower():
-                    return True
-        return False
     
-      
+    def lookup_entry(self):
+        if self.check_website_length():
+            
+            return
+        self.check_email_match()
+    
+    def check_email_match(self):
+        website = self.website_entry.get().strip()
+        data = self.read_entries_from_file()
+
+        if website in data:
+            self.matching_entries = data[website]
+            self.process_matching_entries()
+        else:
+            tk.messagebox.showinfo(title="No Match Found", message="No entries found for the website.")
+
+
+     
+    def process_matching_entries(self):
+        if len(self.matching_entries) == 1:
+            self.insert_matching_entry(self.matching_entries[0])
+            tk.messagebox.showinfo(title="Match Found", message="Match found for the website.")
+        elif len(self.matching_entries) > 1:
+            self.open_selection_window()
+
+
+
+   
+        
+
+    def add_entry(self):
+        website = self.website_entry.get().strip()
+        email = self.email_entry.get().strip()
+        password = self.password_entry.get().strip()
+
+        if not website or not email or not password:
+            tk.messagebox.showinfo(title="Error!", message="Please don't leave any fields empty!")
+            return
+
+        data = self.read_entries_from_file()
+
+        # Check if the website already exists in data
+        if website not in data:
+            data[website] = []
+
+        # Check for duplicate email under the same website
+        if any(entry["email"] == email for entry in data[website]):
+            tk.messagebox.showinfo(title="Duplicate Entry", message="This email for the website already exists!")
+            return
+
+        # Add the new email and password to the website's list
+        data[website].append({"email": email, "password": password})
+        self.write_entries_to_file(data)
+
+        # Clear the input fields
+        self.website_entry.delete(0, tk.END)
+        self.password_entry.delete(0, tk.END)
+        self.email_entry.delete(0, tk.END)
+        self.email_entry.insert(0, "oferteevelin1996.com")
+        self.website_entry.focus()
+
+    
+    def read_entries_from_file(self):
+        try:
+            with open("29.Day_29/data.json", mode="r") as file:
+                return json.load(file)
+        except (FileNotFoundError, json.decoder.JSONDecodeError):
+            
+            return {}  # Return an empty dictionary if file doesn't exist or is empty
+
+    def write_entries_to_file(self, data):
+        with open("29.Day_29/data.json", mode="w") as file:
+            json.dump(data, file,indent=4)
+
+       
+    
+    
+  
     def ui_setup(self):
         self.title("Password Manager")
         self.resizable(False, False)
@@ -193,7 +222,7 @@ class PasswordManager(tk.Tk):
         self.add_entry_button.grid(column=1, row=4)
         
         self.lookup_entry_button = tk.Button(text="Lookup Entry", width=15, command=self.lookup_entry,font=("arial", 12, 'bold'), bg="black", fg="white", highlightthickness=0,activebackground="black", activeforeground="black",border=0, highlightbackground='black', highlightcolor='black')
-        
+        self.clear_entries_button = tk.Button(text="Clear Entry", width=15, command=self.clear_entries,font=("arial", 12, 'bold'), bg="black", fg="white", highlightthickness=0,activebackground="black", activeforeground="black",border=0, highlightbackground='black', highlightcolor='black')
         
         
         self.website_text_label = self.canvas.create_text(200, 350, text="Website:", font=("Arial", 12, "bold"), fill="white")
@@ -203,14 +232,15 @@ class PasswordManager(tk.Tk):
         
         
         
-        
+        self.canvas.create_window(370, 500, window=self.clear_entries_button)
+        self.canvas.create_window(250, 500, window=self.add_entry_button)
         self.canvas.create_window(350, 350, window=self.website_entry)
         self.canvas.create_window(350, 400, window=self.email_entry)
         self.canvas.create_window(310, 450, window=self.password_entry)
         self.canvas.create_window(470, 450, window=self.generate_password_button)
-        self.canvas.create_window(300, 500, window=self.add_entry_button)
         self.canvas.create_window(470, 350, window=self.lookup_entry_button)
  
+
 
 
 if __name__ == "__main__":
