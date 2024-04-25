5abc4ad97f861634943a492d634b35f2e48a8688
Diaconu Evelin <diaconuevelindanut@gmail.com>
2024-04-25 12:55:45 +0300

    Removing a file from git version control

--- 39..Day_39/main.py
+++ 39..Day_39/main.py
@@ -7,7 +7,6 @@ import os
 import requests
 import pandas as pd
 
-info_found_in_sheet = "https://api.sheety.co/d227ad5bce98690ad95c1e66af319197/flightDeals/prices"
 
 
 json_data_sheeety = requests.get(url=info_found_in_sheet).json()
@@ -16,8 +15,4 @@ dataframe_sheet = pd.json_normalize(json_data_sheeety["prices"])
 #print(dataframe_sheet)
 
 from flight_search import FlightSearch
-
-flight_info = FlightSearch()
-flight_data = flight_info.get_flight_info("Bucharest")
-print(flight_data)
 
