e571689f511c688f0ef2959663b2086ecaffa38a
Diaconu Evelin <diaconuevelindanut@gmail.com>
2024-04-25 12:57:24 +0300

    Changes to main

--- 39..Day_39/main.py
+++ 39..Day_39/main.py
@@ -9,10 +9,6 @@ import pandas as pd
 
 
 
-json_data_sheeety = requests.get(url=info_found_in_sheet).json()
-
-dataframe_sheet = pd.json_normalize(json_data_sheeety["prices"])
-#print(dataframe_sheet)
 
 from flight_search import FlightSearch
 
